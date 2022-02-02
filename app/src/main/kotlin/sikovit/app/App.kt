da05a072724b4536f825c17ef1e7dbf82edee865
Kai Chen <kai@sorrentocorp.com>
2022-02-01 18:00:39 -0800

    feat: can pass starting url to app

--- app/src/main/kotlin/sikovit/app/App.kt
+++ app/src/main/kotlin/sikovit/app/App.kt
@@ -1,17 +1,30 @@
 package sikovit.app
 
+import javafx.stage.Stage
 import tornadofx.*
 
 fun main(args: Array<String>) {
     launch<Main>(args)
 }
 
-class Main: App(MainView::class)
+class Main: App(MainView::class) {
+    override fun start(stage: Stage) {
+        parseArgs()
+        super.start(stage)
+    }
 
-class MainView: View() {
+    private fun parseArgs() {
+        val lastArgIndex = parameters.raw.lastIndex
+        val location = parameters.raw.getOrElse(lastArgIndex) { "https://www.duckduckgo.com" }
+        config["window.location"] = location
+        config.save()
+    }
+}
+
+class MainView: View("Main Web View") {
     override val root = vbox {
         webview {
-            engine.load("https://www.duckduckgo.com")
+            engine.load(app.config.string("window.location"))
         }
     }
 }
