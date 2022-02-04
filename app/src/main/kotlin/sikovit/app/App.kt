f1a44bcc6f13a1fb09a0e481eb86ac9a1e0c0516
Kai Chen <kai@sorrentocorp.com>
2022-02-04 15:41:46 -0800

    wip: add logging

--- app/src/main/kotlin/sikovit/app/App.kt
+++ app/src/main/kotlin/sikovit/app/App.kt
@@ -2,6 +2,10 @@ package sikovit.app
 
 import javafx.stage.Stage
 import tornadofx.*
+import java.util.logging.ConsoleHandler
+import java.util.logging.FileHandler
+import java.util.logging.Level
+import java.util.logging.LogManager
 
 fun main(args: Array<String>) {
     launch<Main>(args)
@@ -10,7 +14,16 @@ fun main(args: Array<String>) {
 class Main: App(MainView::class) {
     override fun start(stage: Stage) {
         parseArgs()
+        // configLogger()
         super.start(stage)
+    }
+
+    private fun configLogger() {
+        val logger = LogManager.getLogManager().getLogger("")
+        logger.level = Level.FINEST
+        val logHandler = FileHandler("webkit.log")
+        logHandler.level = Level.ALL
+        logger.addHandler(logHandler)
     }
 
     private fun parseArgs() {
